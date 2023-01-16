cbde74544959b49131ab98e4ee7d4b9f99a4241a
monika <monika@orl25.de>
2023-01-16 20:59:00 +0100

    Make first plots

--- bau-hu.R
+++ bau-hu.R
@@ -2,6 +2,7 @@
 # dazu motivieren, auf das Fahrrad umzusteigen?
 
 library(dplyr)
+library(ggplot2)
 
 # relevante Zielgruppe: Nichtradfahrer, die von den eigenen Fähigkeiten (C1.) 
 # und der eigenen Ausstattung (C2.) her Fahrrad fahren könnten und die auch 
@@ -10,7 +11,7 @@ library(dplyr)
 non_bike <- read.csv("non_bike.csv")
 # crit_col <- c(9)
 # crit_col <- c(8,9,10,11,12,13,14,15,16,32,33,34,35)
-# Ohne alle diese Leute bleiben nur 7 übrig. Schärfere Kriterien:
+# Ohne alle diese Leute bleiben zu wenige übrig. Schärfere Kriterien:
 crit_col <- c(11,33)
 # Nur Entfernung und körperliche Einschränkung
 pot_bike <- non_bike
@@ -31,22 +32,44 @@ colnames(pot_bike)[20] <- 'Dach'
 colnames(pot_bike)[21] <- 'Dusche'
 colnames(pot_bike)[22] <- 'Ladestation'
 
-adl <- pot_bike[pot_bike[3] == "Adlershof",c(18,19,20,21,22)]
+adl <- pot_bike[pot_bike[3] == "Adlershof",]
 ms <- pot_bike[pot_bike[3] == "Süd/Mitte",]
 no <- pot_bike[pot_bike[3] == "Nord",]
-
-y <- c("Trifft nicht zu","Trifft eher nicht zu","Weiß nicht","Trifft eher zu","Trifft zu")
 
 count_answers <- function(campus) {
   counts <- data.frame("Spinde" = table(campus$Spinde), "Diebstahl" = table(campus$Diebstahl),
                            "Dach" = table(campus$Dach), "Dusche" = table(campus$Dusche),
                            "Ladestation" = table(campus$Ladestation))
+  # table(...) erzeugt den Index immer mit, deshalb werden überflüssige
+  # Index-Spalten hier entfernt:
   counts <- counts %>% select(-one_of('Diebstahl.Var1', 'Dach.Var1', 'Dusche.Var1', 'Ladestation.Var1')) 
-  counts <- counts[c(3, 1, 5, 2, 4), ] 
+  counts <- counts[c(3, 1, 5, 2, 4), ] # Bewertungsskala in sinnvolle Reihenfolge bringen
+  rownames(counts) <- counts[ ,1]
+  counts <- as.data.frame(t(counts))
+  counts <- counts[-c(1), ]
+  counts <- cbind(Maßnahme = rownames(counts), counts)
+  rownames(counts) <- 1:nrow(counts)
   return(counts)
   }
 
+# Anpassung des Formats zum Plotten:
 adl_counts <- count_answers(adl)
-ms_counts <- count_answers(ms)
-no_counts <- count_answers(no)
 
+adl_counts$campus <- rep("Adlershof",nrow(adl_counts))
+ms_counts <- count_answers(ms)
+ms_counts$campus <- rep("Mitte",nrow(ms_counts))
+no_counts <- count_answers(no)
+no_counts$campus <- rep("Nord",nrow(no_counts))
+
+counts_joined = rbind(adl_counts, ms_counts, no_counts)
+colnames(counts_joined)[6] <- 'Trifft_zu'
+colnames(counts_joined)[5] <- 'Trifft_eher_zu'
+
+counts_joined$Trifft_zu <- as.numeric(counts_joined$Trifft_zu)
+counts_joined$Trifft_eher_zu <- as.numeric(counts_joined$Trifft_eher_zu)
+
+counts_joined$positiv <- counts_joined$Trifft_zu + counts_joined$Trifft_eher_zu
+
+# Plotten:
+ggplot(counts_joined, aes(fill=Maßnahme, y=positiv, x=campus)) + 
+  geom_bar(position='dodge', stat='identity')
