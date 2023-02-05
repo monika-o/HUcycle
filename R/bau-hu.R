5c00b2073d2b7b470a40af1927da08d218dc1da9
monika <monika@orl25.de>
2023-02-05 16:02:20 +0100

    Simplify and generalise by using ldply function

--- R/bau-hu.R
+++ R/bau-hu.R
@@ -1,80 +1,76 @@
 # Welche baulichen Maßnahmen an der HU könnten am meisten Nicht-Radfahrer
 # dazu motivieren, auf das Fahrrad umzusteigen?
 
-library(dplyr)
+library(plyr)
 library(ggplot2)
 library("reshape2")
 # library(tidyverse)
 
-# relevante Zielgruppe: Nichtradfahrer, die von den eigenen Fähigkeiten (C1.) 
+
+count_answers <- function(campusdf, variablelist) {
+  campusdf_filtered <- campusdf[ , names(campusdf) %in% variablelist]
+  counted <- ldply( campusdf_filtered, function(x) data.frame( table(x) )  )
+
+  colnames(counted)[1] <- "Maßnahme"
+  colnames(counted)[2] <- "variable"
+  colnames(counted)[3] <- "value"
+  return(counted)
+  }
+
+plot_grouped_stacked_barplot <- function(df, variablelist){
+  # The df must contain the variables HU_Spinde, HU_Diebstahl, HU_Dach, HU_Dusche and HU_Ladestation
+  # for the 3 different campi. The campi must be named in a column called "Campus".
+  
+  adl <- df[df["Campus"] == "Adlershof",]
+  ms <- df[df["Campus"] == "Süd/Mitte",]
+  no <- df[df["Campus"] == "Nord",]
+
+  adl_counts <- count_answers(adl, variablelist)
+  adl_counts$campus <- rep("Adlershof",nrow(adl_counts))
+  ms_counts <- count_answers(ms, variablelist)
+  ms_counts$campus <- rep("Mitte",nrow(ms_counts))
+  no_counts <- count_answers(no, variablelist)
+  no_counts$campus <- rep("Nord",nrow(no_counts))
+
+  counts_joined = rbind(adl_counts, ms_counts, no_counts)
+
+  counts_joined %>%
+    mutate(variable = fct_relevel(variable, "Trifft nicht zu", "Trifft eher nicht zu", "Weiß nicht", "Trifft eher zu", "Trifft zu"),
+  #         .id = fct_reorder(.id, val)
+  ) %>%
+  ggplot(counts_joined, mapping = aes(x=Maßnahme, y=value, fill=variable) ) + 
+    geom_col() + facet_grid(.~campus) +
+    theme(axis.text.x = element_text(angle = 90)) +
+    scale_fill_manual(values=c("#949494","#A8A8A8", "#BABABA", "#E58A19", "#E55C19"))
+  }
+
+
+### Vorbereitung der Daten
+# 1. relevante Zielgruppe: Nichtradfahrer, die von den eigenen Fähigkeiten (C1.) 
 # und der eigenen Ausstattung (C2.) her Fahrrad fahren könnten und die auch 
-# von den “weiteren Faktoren” (C5) nicht abgehalten werden
+# von den “weiteren Faktoren” (C5) nicht abgehalten werden.
+# Problem: Dabei bleiben zu wenige übrig; deshalb habe ich die Kriterien auf
+# die Entfernung und körperliche Einschränkungen reduziert.
 
 non_bike <- read.csv("preprocessed/non_bike_cleaned.csv")
-# crit_col <- c(9)
-# crit_col <- c(8,9,10,11,12,13,14,15,16,32,33,34,35)
-# Ohne alle diese Leute bleiben zu wenige übrig. Schärfere Kriterien:
 crit_col <- c("selbst_zu_weit", "selbst_körperlicheEinschränkung")
-# Nur Entfernung und körperliche Einschränkung
 pot_bike <- non_bike
 
 for (i in crit_col)
 {
   pot_bike <- pot_bike[pot_bike[i] == "Trifft nicht zu" | 
                          pot_bike[i] == "Trifft eher nicht zu", 
-                      ]
-  }
+  ]
+}
+
+# 2. relevante Zielgruppe: Radfahrer*innen
+
+bike <- read.csv("preprocessed/bike_cleaned.csv")
 
 
+### Verarbeitung und Plotten mittels der obigen Funktionen
+# variablelist <- c("Diensträder", "Info_Werkstatt", "Umgebungsplan")
+variablelist <- c("HU_Dach", "HU_Diebstahl", "HU_Duschen",  "HU_Ladestation", "HU_Spinde")
+plot_grouped_stacked_barplot(pot_bike, variablelist)
 
-count_answers <- function(campusdf) {
-  counts <- data.frame("Spinde" = table(campusdf$HU_Spinde), "Diebstahl" = table(campusdf$HU_Diebstahl),
-                           "Dach" = table(campusdf$HU_Dach), "Dusche" = table(campusdf$HU_Dusche),
-                           "Ladestation" = table(campusdf$HU_Ladestation))
-  # table(...) erzeugt den Index immer mit, deshalb werden überflüssige
-  # Index-Spalten hier entfernt:
-  counts <- counts %>% select(-one_of('Diebstahl.Var1', 'Dach.Var1', 'Dusche.Var1', 'Ladestation.Var1')) 
-  counts <- counts[c(3, 1, 5, 2, 4), ] # Bewertungsskala in sinnvolle Reihenfolge bringen
-  rownames(counts) <- counts[ ,1]
-  counts <- as.data.frame(t(counts))
-  counts <- counts[-c(1), ]
-  counts <- cbind(Maßnahme = rownames(counts), counts)
-  rownames(counts) <- 1:nrow(counts)
-  counts$Maßnahme <- as.character(counts$Maßnahme)
-  counts$Maßnahme <- sapply(strsplit(as.character(counts$Maßnahme), ".F"), `[`, 1)
-  
-  return(counts)
-  }
-
-
-adl <- pot_bike[pot_bike["Campus"] == "Adlershof",]
-ms <- pot_bike[pot_bike["Campus"] == "Süd/Mitte",]
-no <- pot_bike[pot_bike["Campus"] == "Nord",]
-
-adl_counts <- count_answers(adl)
-adl_counts$campus <- rep("Adlershof",nrow(adl_counts))
-ms_counts <- count_answers(ms)
-ms_counts$campus <- rep("Mitte",nrow(ms_counts))
-no_counts <- count_answers(no)
-no_counts$campus <- rep("Nord",nrow(no_counts))
-
-counts_joined = rbind(adl_counts, ms_counts, no_counts)
-
-
-# counts_joined$positiv <- counts_joined$Trifft_zu + counts_joined$Trifft_eher_zu
-
-# aggregoert positiv plotten:
-# ggplot(counts_joined, aes(fill=Maßnahme, y=positiv, x=campus)) + 
-#  geom_bar(position='dodge', stat='identity')
-
-
-data_long1 <- melt(counts_joined,
-                   id.vars = c("Maßnahme", "campus"))
-
-data_long1$value <- as.numeric(data_long1$value)
-
-ggplot(data_long1, aes(x=Maßnahme, y=value, fill=variable) ) + 
-  geom_col() + facet_grid(.~campus) +
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(values=c("#949494","#A8A8A8", "#BABABA", "#E55C19", "#E58A19"))
-
+# plot_grouped_stacked_barplot(bike, variablelist)
